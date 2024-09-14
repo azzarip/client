314e6a4c7c70c4c732e36063b8f4243d3ed92919
azzarip <azzarip@users.noreply.github.com>
2024-09-14 09:59:46 +0000

    Fix styling

--- src/ClientServiceProvider.php
+++ src/ClientServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace Azzarip\Client;
 
-use Livewire\Livewire;
+use Azzarip\Client\CookieConsent\ConsentManager;
 use Azzarip\Client\Theme\Theme;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Config;
+use Livewire\Livewire;
 use Spatie\LaravelPackageTools\Package;
-use Azzarip\Client\CookieConsent\ConsentManager;
-use Illuminate\Cookie\Middleware\EncryptCookies;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class ClientServiceProvider extends PackageServiceProvider
