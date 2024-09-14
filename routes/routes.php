0bf564d0efccfca303dcb6e10bb426da288059b1
Azzarip <paride21azzari@gmail.com>
2024-09-14 11:24:18 +0200

    route have domain key

--- routes/routes.php
+++ routes/routes.php
@@ -5,10 +5,11 @@ use Azzarip\Utilities\Http\Middleware\DomainKey;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/privacy-policy', PolicyController::class)->name('privacy-policy');
-Route::get('/cookie-policy', PolicyController::class)->name('cookie-policy');
+Route::middleware(DomainKey::class)->group(function () {
 
-// Route::middleware(DomainKey::class)->group(function () {
+    Route::get('/privacy-policy', PolicyController::class)->name('privacy-policy');
+    Route::get('/cookie-policy', PolicyController::class)->name('cookie-policy');
+
 //     Route::get('/sitemap.xml', function () {
 //         $key = request()->get('domainKey');
 //         $xml = File::get(storage_path("app/sitemaps/$key.xml"));
@@ -28,4 +29,4 @@ Route::get('/cookie-policy', PolicyController::class)->name('cookie-policy');
 
 //         return response($ico, 200)->header('Content-Type', 'image/vnd.microsoft.icon');
 //     });
-// });
+});
