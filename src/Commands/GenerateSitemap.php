2e167ff1cbfac45a519359ef16577c38d1b429e8
Azzarip <paride21azzari@gmail.com>
2024-09-27 15:06:51 +0200

    sitemaps are in storage app

--- src/Commands/GenerateSitemap.php
+++ src/Commands/GenerateSitemap.php
@@ -31,7 +31,7 @@ class GenerateSitemap extends Command
     {
         foreach (config('domains') as $key => $domain) {
 
-            $directory = storage_path("framework/sitemaps");
+            $directory = storage_path("app/sitemaps");
             if (!File::exists($directory)) {
                 File::makeDirectory($directory, 0755, true);
             }
