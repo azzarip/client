0bf509fb2540c8e93a5f4024f7c2cb2b84542ce7
Azzarip <paride21azzari@gmail.com>
2025-06-09 11:16:35 +0200

    not found sitemap return 404

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Azzarip\Client\Http\Controllers\PolicyController;
+use Azzarip\Client\Http\Controllers\SitemapController;
 use Azzarip\Client\Http\Middleware\DomainKey;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -10,12 +11,7 @@ Route::middleware(DomainKey::class)->group(function () {
     Route::get('/privacy-policy', PolicyController::class)->name('privacy-policy');
     Route::get('/cookie-policy', PolicyController::class)->name('cookie-policy');
 
-    Route::get('/sitemap.xml', function () {
-        $key = request()->get('domainKey');
-        $xml = File::get(storage_path("app/sitemaps/$key.xml"));
-
-        return response($xml, 200)->header('Content-Type', 'application/xml');
-    });
+    Route::get('/sitemap.xml', SitemapController::class);
 
     Route::get('/favicon.ico', function () {
         $key = request()->get('domainKey');
